643745a0e0345bc793db82efacc5ddc0be6aee92
Adam Weissman <adam@comedytragedyepic.com>
2019-11-16 10:55:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,33 +1,27 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
-  def initialize(id:, name:, type:, db:)
-    @id = id
-    @name = name
-    @type = type
-    @db = db  
+  
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize (id=nil, name=nil, type=nil, db=nil)
+    @id=id 
+    @name=name 
+    @type=type
+    @db=db
   end
   
-  def self.save(name,type,db)
+  def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type) 
+      INSERT INTO pokemon (name, type)
       VALUES (?, ?)
     SQL
- 
     db.execute(sql, name, type)
-    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")
   end
   
   def self.find(id, db)
-    sql = <<-SQL
-      SELECT *
-      FROM pokemon
-      WHERE id = ?
-      LIMIT 1
-    SQL
- 
-    db.execute(sql, id).map do |row|
-      pokemon = self.new(id:row[0],name:row[1],type:row[2], db:row[3])  
-    end.first
+    sql = "SELECT * FROM pokemon WHERE id = ?"
+    result = db.execute(sql, id).first
+    Pokemon.new(result[0], result[1], result[2])
   end
-  
 end
+  
